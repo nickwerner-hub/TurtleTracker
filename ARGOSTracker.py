e0ec53d7a852bca8a32b35f9565e79ee69360037
nickwerner-hub <nicholas.werner@duke.edu>
2019-09-30 12:11:08 -0400

    Adds information

---
+++ ARGOSTracker.py
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------
+# ARGOSTracker.py
+#
+# Description: Parses a line of ARGOS tracking data 
+#
+# Created by: Nicholas Werner (nicholas.werner@duke.edu)
+# Created on: Fall 2019
+#--------------------------------------------------------------
